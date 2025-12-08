4495e799d2c156fb0815b9912045249c9181bedb
Olcan EBREM <olcanebrem@gmail.com>
2025-12-08 23:28:45 +0300

    removing everything except index

--- src/components/RiveMeeple.tsx
+++
@@ -1,8 +0,0 @@
-import Rive from '@rive-app/react-canvas';
-
-export const Simple = () => (
-  <Rive
-    src="https://cdn.rive.app/animations/vehicles.riv"
-    stateMachines="bumpy"
-  />
-);
--- src/components/ui/MWC/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'md-filled-button': any;
-    'md-elevated-card': any;
-    'md-card-header': any;
-    'md-card-content': any;
-    'md-switch': any;
-  }
-}
